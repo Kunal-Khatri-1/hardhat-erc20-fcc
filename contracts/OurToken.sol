fdbcd781499ad479527c2899a7c6b91dd30bdf2b
Kunal Khatri <kunalkhatri137@gmail.com>
2022-10-22 22:17:16 +0530

    constructor bug fixed in openzeppelin

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken is ERC20 {}
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("SampleToken", "SOT") {
+        // minting initial amount of tokens using _mint of openzeppelin ERC-20 _mint function
+        // deployer will have all the tokens in the beginning
+
+        _mint(msg.sender, initialSupply);
+    }
+}
